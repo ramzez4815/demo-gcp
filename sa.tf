66f044c16631d7c3dd2872996f86a032920c650b
Ram <ramses_perez@epam.com>
2021-07-08 16:38:06 -0500

    Adding gitignore and roles to sa

--- sa.tf
+++ sa.tf
@@ -1,10 +1,10 @@
 resource "google_service_account" "gce-sa" {
-  account_id   = "gce_sa"
-  display_name = "gce-sa"
+  account_id   = "gce-sa"
+  display_name = "gce_sa"
 }
 
 resource "google_project_iam_member" "gce-iam" {
   project = "epam-001"
-  role    = "pubsub-resource"
-  member  = "serviceAccount:${google_service_account.gce_sa.email}"
+  role    = "projects/epam-001/roles/pubsub_resource"
+  member  = "serviceAccount:${google_service_account.gce-sa.email}"
 }
