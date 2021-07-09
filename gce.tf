581df5041d6a232f0f2fc05c21a6f574cde456a5
Ram <ramses_perez@epam.com>
2021-07-09 11:45:23 -0500

    Simplify gcloud command

--- gce.tf
+++ gce.tf
@@ -13,7 +13,7 @@ resource "google_compute_instance" "vm-instance" {
     metadata = {
         startup-script = <<SCRIPT
         echo "#!/bin/bash" > /home/ubuntu/task.sh
-        echo '/snap/bin/gcloud pubsub subscriptions pull task01-subscription --auto-ack --limit=10 --format="json(message.data)" | grep -E "data.*:" | awk -F \" '\''{ print $4 }'\' ' | base64 -d > /home/ubuntu/output_`date +%d-%m-%Y_%H:%M:%S`.json' >> /home/ubuntu/task.sh
+        echo '/snap/bin/gcloud pubsub subscriptions pull task01-subscription --auto-ack --limit=10 --format='\''value[separator=","](DATA)'\'' > /home/ubuntu/output_`date +%d-%m-%Y_%H:%M:%S`.json' >> /home/ubuntu/task.sh
         echo "/snap/bin/gsutil cp /home/ubuntu/output* gs://task01-bucket/" >> /home/ubuntu/task.sh
         echo "rm -rf /home/ubuntu/output* " >> /home/ubuntu/task.sh
         chmod +x /home/ubuntu/task.sh
