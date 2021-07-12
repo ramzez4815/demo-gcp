6b88ea9dc7d793abbc127a311e028849d0afed15
Ram <ramses_perez@epam.com>
2021-07-12 12:51:47 -0500

    Adding startup script

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 FILE=/home/ubuntu/flock.tmp
 /usr/bin/touch $FILE
 if [ ! -f "${FILE}" ]; then 
-    crontab -l ; echo '*/5 * * * * /snap/bin/gcloud pubsub subscriptions pull task01-subscription --limit=10 --format='\''value(DATA)'\'' ; /snap/bin/gsutil cp /home/ubuntu/output* gs://task01-bucket/ ;  rm -rf /home/ubuntu/output* ' | crontab -
+    crontab -l ; echo '*/5 * * * * /snap/bin/gcloud pubsub subscriptions pull task01-subscription --limit=10 --format='\''value(DATA)'\'' > /home/ubuntu/output_`date +%d-%m-%Y_%H:%M:%S`.json ; /snap/bin/gsutil cp /home/ubuntu/output* gs://task01-bucket/ ;  rm -rf /home/ubuntu/output* ' | crontab -
 fi 
