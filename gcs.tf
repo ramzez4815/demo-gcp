abff32e6cdc4dcda16df3d2ab5895cc2bed41c74
Ram <ramses_perez@epam.com>
2021-07-19 00:06:17 -0500

    Fixing incorrect variable

--- gcs.tf
+++ gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  name                        = var.scheduler_time_zone
+  name                        = var.gcs_name
   location                    = var.gcs_region
   project                     = var.gcp_project_id
   uniform_bucket_level_access = true
